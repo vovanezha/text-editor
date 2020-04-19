99b060fcf7bba7ba7f87472d0ab7b9b45e9de981
Vladimir.Nejelschii <vladimir.nejelschii@itechart-group.com>
2020-04-19 19:40:59 +0300

    Save data to localstorage

--- src/main/main.js
+++ src/main/main.js
@@ -1,16 +1,23 @@
-function createPlaceholder() {
-  const placeholder = document.createElement("span");
-  placeholder.classList.add("placeholder");
-  placeholder.append("Just start typing...");
-  
-  return placeholder;
-}
+import getFromLocalStorage from "../../lib/get-from-local-storage";
 
 const editables = document.querySelectorAll(".editable");
+const savedContent = getFromLocalStorage("editable-content");
+const content = savedContent || {};
 
 editables.forEach((editable, index) => {
   // TODO: set focus on the last edited element
   if (index === 0) {
     editable.focus();
   }
+  
+  if (savedContent[editable.id]) {
+    editable.innerHTML = savedContent[editable.id];
+  }
+  
+  editable.addEventListener("input", (event) => {
+    const newValue = event.target.innerHTML;
+    content[editable.id] = newValue;
+    
+    localStorage.setItem("editable-content", JSON.stringify(content));
+  });
 });
